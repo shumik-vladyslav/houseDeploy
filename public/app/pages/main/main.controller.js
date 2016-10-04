ec5cdb1b3d6511d579c43cc6a7aac81b23c596f2
shumik-vladyslav <shumik.vladyslav@gmail.com>
2016-10-04 15:43:02 +0300

    g

--- public/app/pages/main/main.controller.js
+++ public/app/pages/main/main.controller.js
@@ -3,16 +3,7 @@
 
   angular
     .module('app')
-    .controller('MainController', MainController).controller('AppCtrl', function ($scope, $timeout, $mdSidenav) {
-      $scope.toggleLeft = buildToggler('left');
-      $scope.toggleRight = buildToggler('right');
-
-      function buildToggler(componentId) {
-        return function () {
-          $mdSidenav(componentId).toggle();
-        }
-      }
-    });
+    .controller('MainController', MainController)
 
   /** @ngInject */
   function MainController($state, $rootScope, $http, TempDialogService,AuthService) {
@@ -25,16 +16,23 @@
       return comfort_status.profiles[comfort_status.profiles.length - 1].set_point;
     };
 
-    vm.edit = function (item) {
+    vm.schedule = function (item) {
       console.log(item)
-      TempDialogService.showDialog(item);
-      //$state.go('history', {applianceId: item.appliance_id})
+      //TempDialogService.showDialog(item);
+      $state.go('schedule', {applianceId: item.appliance_id})
     }
 
-    vm.swapComfort = function (item, flag) {
+     vm.edit = function (item) {
+      $state.go('schedule', {applianceId: item.appliance_id})
+    }
+      vm.history = function (item) {
+      $state.go('history', {applianceId: item.appliance_id})
+    }
+
+    vm.swapComfort = function (item) {
       console.log(item)
 
-      item.comfort_active = !flag;
+      item.comfort_active = !item.comfort_active;
       console.log(item)
 
       $http.post('http://sse.logicenergy.com/manager/sse/api/v1/appliance/' + item.appliance_id + '?access_token=' + vm.token, { "appliance": item }).then(function (ress) {
